9ab2e5fe11a8ee0926265d583324563ec6542895
Amun Kharel <akharel@cs.unm.edu>
2019-08-24 08:31:55 -0600

    First Commit

---
+++ src/nyTileGame/Main.java
@@ -0,0 +1,20 @@
+package nyTileGame;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        System.out.println("Hello WOrld");
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}

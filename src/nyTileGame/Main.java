f68329afaddcb1d6bb78abeda34a74e408e73076
Amun Kharel <akharel@cs.unm.edu>
2019-08-25 16:54:11 -0600

    Remove clunky test code

--- src/nyTileGame/Main.java
+++ src/nyTileGame/Main.java
@@ -70,61 +70,6 @@ public class Main extends Application {
             System.out.println(" pressed " + event.getX() + " " + event.getY());
         });
 
-        /*int numberOfTiles = 26;
-
-        BorderPane bp = new BorderPane();
-
-
-        GridPane grid = new GridPane();
-
-
-        Text current_score = new Text();
-
-        current_score.setText("Current Score is 19");
-        current_score.setFont(Font.font("Verdana", 20));
-
-        Text longest_streak = new Text();
-
-        longest_streak.setText("Longest Run is 100");
-        longest_streak.setFont(Font.font("Verdana", 20));
-
-        bp.setCenter(grid);
-
-        bp.setLeft(current_score);
-        bp.setRight(longest_streak);
-
-        for (int row = 0; row < 7; row++) {
-            for (int col = 0; col < 7; col++) {
-                Circle circ = new Circle();
-                circ.setCenterX(35);
-                circ.setCenterY(35);
-                circ.setRadius(10);
-                circ.setFill(Color.RED);
-                Rectangle rec = new Rectangle();
-                rec.setWidth(70);
-                rec.setHeight(70);
-                rec.setFill(Color.valueOf("#feb"));
-                rec.setStroke(Color.BLACK);
-                GridPane.setRowIndex(rec, row);
-                GridPane.setColumnIndex(rec, col);
-                grid.getChildren().addAll(rec, circ);
-            }
-        }
-
-
-        Scene scene = new Scene(bp, 1000, 800);
-
-        primaryStage.setTitle("Tile Game");
-        primaryStage.setMaximized(true);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-
-        grid.addEventHandler(MouseEvent.MOUSE_PRESSED, event -> {
-            System.out.println(" pressed " + event.getX() + " " + event.getY());
-        }); */
-
-
-
     }
 
     public static void main(String[] args) {
